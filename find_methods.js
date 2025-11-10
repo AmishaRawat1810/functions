1197a606ef14dc14e08e1eb24fc98b05ce1cc281
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2025-11-10 12:29:17 +0530

    frequency of element

---
+++ find_methods.js
@@ -0,0 +1,15 @@
+// INPUT : [2,2,3,4,5,2,4]
+// OUTPUT : [[2, 3], [3, 1], [4, 2], [5, 1]]
+
+const occurences = (accumulator, element) => {
+  const occurence = accumulator.find(x => x[0] === element);
+  if (occurence !== undefined) {
+    occurence[1]++;
+  } else {
+    accumulator.push([element, 1]);
+  }
+
+  return accumulator;
+};
+
+console.log([2, 2, 3, 4, 5, 2, 4].reduce(occurences, []));
